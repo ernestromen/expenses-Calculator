08756052ad5e5b6fa21dde8ce8b89aa9f2ed624b
Ernest Romen <ernestromen1996@gmail.com>
2021-04-23 16:18:28 +0300

    insert complete still problem with confirm condition

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <?php
 
 class DB{
-
+//pdo connection to be inherited
 private $dns;
 private $user;
 private $password;
 public $pdo;
+
 
 
   public function connect(){
@@ -18,32 +19,29 @@ public $pdo;
     return $this;
   }
 
+  
+  function __construct() {
+    $this->connect();
+}
 
 
-  public static function insert(){
-$test = 'inside insert';
-var_dump($test);
-$attmept = new DB();
-die;
-//     $sql = "INSERT INTO (amount) VALUES($res)";
-//  $this->pdo->query($sql);
-// var_dump($final);
+  
+  
 
-  }
+
 }
  
 class Validation extends DB{
-
+     //validation 
   public  $errors = [
         'input' => ''
         
         ];
-        
+    
       public function validate(){
-
+    
 
         if(isset($_POST['submit'])){
-// var_dump(empty($_POST['input']));
 
           if(empty($_POST['input']) ){
             $this->errors['input'] = '<br>'.'* the input is not valid';
@@ -53,36 +51,38 @@ class Validation extends DB{
 
               $this->errors['input'] = '<br>'.'* the input must be a number';
             }
+            //if the input passes all the validation
             else{
-            // $tmp = 'hello';
-            // $tmp = json_encode($tmp);
-
-            // echo json_encode($_POST['input']);
-            // echo json_encode($_GET['input']);
+   
              $result = $_POST['input'];
-             $a = 'from php with love';
             //  var_dump($result);
              if($result){
               // $a = 'from php with love';
 echo '<script>
 
     var res = confirm("Hello! I am an alert box!!");
+    console.log(res);
               // if not true
               if((res)){
 
 document.cookie = "confirm =1;";
                 // alert("income recored");
                 
-              }else{
+              }else if(!(res)){
 
+                document.cookie = "confirm =0;";
 
 
               }
 </script>';
 echo '<pre>';
 // var_dump($_COOKIE);
-if($_COOKIE['confirm'] ==1){
-$this->insert();
+if($_COOKIE['confirm'] =='1'){
+  // var_dump($this->connect());
+  // die;
+$this->insert($result);
+}else{
+var_dump('in else if');
 }
 // foreach($_COOKIE as $key=>$value)
 // {
@@ -95,25 +95,6 @@ $this->insert();
           
 
              }
-
-
-
-
-            //practicing queries;!  
-
-            // $sql = "CREATE TABLE salary(
-            //   id int NOT NULL AUTO_INCREMENT,
-            //   amount int NOT NULL,
-            //   created_at datetime,
-            //    PRIMARY KEY (id)
-            // )";
-//             if($this->pdo->query($sql)){
-// echo 'yes';
-
-//             }else{
-//               echo'no';
-//             };
-
             
             }
             
@@ -130,21 +111,58 @@ $this->insert();
 }
 
 
+class CRUD extends Validation{
+//does all the actions
+private $db;
+public function __construct($db){
+// global $db;
+$this->db = $db;
+// var_dump($this->db);
+}
+
+  public  function insert($res){
+        $sql = "INSERT INTO expenses (amount) VALUES ($res)";
+
+    // var_dump($this->db->pdo);
+    var_dump($res);
+    var_dump($this->db->pdo->query($sql));
+    // $test = 'inside insert';
+    // var_dump($test);
+    // var_dump($this->db);
+    // $attmept = new DB();
+    // var_dump($attmept);
+
+    
+      }
+
+      public function show(){
+$sql = "SELECT * FROM expenses";
+$result =  $this->db->pdo->query($sql);
+foreach($result as $res1){
+
+echo  ($res1['id']);
+}
 
 
-// class Queries{
-// public function insert($pdo,$result){
 
-// $sql = "INSERT INTO (amount) VALUES($result)";
-// $
 
-// }
-// }
+
+      }
+
+}
+
+
+
+
+
+
+
+
 $res = new DB();
 $val = new Validation();
+$crud = new CRUD($res);
 
-$val->connect()->validate();
-
+$crud->connect()->validate();
 
 
 
@@ -160,6 +178,7 @@ $val->connect()->validate();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="mystyle.css">
 </head>
 <body>
 <div id="root"></div>
@@ -172,6 +191,13 @@ $val->connect()->validate();
 
 
 </form>
+
+
+
+<div class="">
+  
+<?php $crud->show();?>
+</div>
 <script type="text/javascript">
 
 
