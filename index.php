26367480e143ba45b80b07d6ba07caf45573ad35
Ernest Romen <ernestromen1996@gmail.com>
2021-05-13 22:51:22 +0300

    g

--- index.php
+++ index.php
@@ -63,10 +63,21 @@ public $cleardb_db = substr($cleardb_url["path"],1);
 
   public function connect(){
                 // "mysql:host='.$host.'; dbname='.$dbname.';"
-    $this->dns = "mysql:host='. $cleardb_server.'; dbname='. $cleardb_db.';";
+    $this->dns = "mysql:host= $cleardb_server; dbname= $cleardb_db;";
     $this->user=$cleardb_username;
     $this->password=$cleardb_password;
+    try {
     $this->pdo = new PDO($this->dns,$this->user,$this->password); 
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    foreach($pdo->query('SELECT title FROM news') as $row) {
+      echo htmlentities($row['title']);
+  }
+  $pdo = null;
+}catch(PDOException $e) {
+  print "Error!: " . $e->getMessage() . "<br/>";
+  die();
+}
+
     return $this;
   }
 
