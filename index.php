6617ee43fafb2da03c6b167e6abf75db7fc692f6
Ernest Romen <ernestromen1996@gmail.com>
2021-05-13 23:28:27 +0300

    g

--- index.php
+++ index.php
@@ -41,8 +41,8 @@ $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
-// var_dump($cleardb_url);
-class DB  {
+var_dump($cleardb_url);
+/*class DB  {
 //heroku shit
 
 
