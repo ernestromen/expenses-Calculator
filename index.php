8382f0f6036f4adfd647ce299d8d90d2ec7b017d
Ernest Romen <ernestromen1996@gmail.com>
2021-05-15 13:23:13 +0300

    k

--- index.php
+++ index.php
@@ -223,7 +223,7 @@ return ($this->result3);
 
 $res = new DB($cleardb_server,$cleardb_db,$cleardb_username,$cleardb_password);
 
-// $val = new Validation();
+$val = new Validation();
 // $crud = new CRUD($res);
 
 // // $crud->connect()->validate();
