99f6e4de45f383dd2a8b98c972e3d2ce225679c2
Ernest Romen <ernestromen1996@gmail.com>
2021-05-16 15:05:41 +0300

    adding validation for input for salary

--- index.php
+++ index.php
@@ -171,7 +171,7 @@ if(isset($_POST['submitSalary'])){
   $this->errors['salaryInput'] = '<br>'.'* the input must be a number';
 
 
-}else if((is_numeric($_POST['source'])){
+}else if(is_numeric($_POST['source']){
   $this->errors['salaryInput'] = '<br>'.'* the input must be text';
 
 }
