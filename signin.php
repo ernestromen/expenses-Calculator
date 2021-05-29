d61354eae95ea9757a80692859b65dff00c4e5b6
Ernest Romen <ernestromen1996@gmail.com>
2021-05-29 16:39:10 +0300

    adding login for users

--- signin.php
+++ signin.php
@@ -33,12 +33,13 @@ var_dump($name);
 var_dump($password);
 
 $sql = "SELECT name,password FROM users WHERE name = '$name' AND password='$password'";
+$result = mysqli_query($conn, $sql);
+
 if($result){
     echo 'result exists';
 }else{
     echo 'it doesnt';
 }
- $result = mysqli_query($conn, $sql);
 var_dump($result);
  // $this->db->pdo->query($sql);
 
