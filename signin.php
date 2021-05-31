05999b527fa87b94cb46fe162439c610ad3730bf
Ernest Romen <ernestromen1996@gmail.com>
2021-05-31 20:38:44 +0300

    500 error from query debug

--- signin.php
+++ signin.php
@@ -71,7 +71,7 @@ $outcome->process();
 <body>
     <h2 style="text-align:center;">SignUp</h2>
     <form style="text-align:center;" method="post" action="">
-<input  tyle="margin-bottom:15px;width:30%;text-align:center;" type="text" name="name" placeholder="name here"><br>
+<input  style="margin-bottom:15px;width:30%;text-align:center;" type="text" name="name" placeholder="name here"><br>
 <input style="margin-bottom:15px;width:30%;text-align:center;" type="password" name="password" placeholder="password here"><br>
 
 
