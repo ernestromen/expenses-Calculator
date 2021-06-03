7ded46b13511e802fa796c4ba18926783f94d901
Ernest Romen <ernestromen1996@gmail.com>
2021-06-03 22:57:07 +0300

    session work

--- index.php
+++ index.php
@@ -212,8 +212,10 @@ return ($this->result);
 
       public function show2(){
 $sql = "SELECT id,purchasetype,SUM(amount) as amount,(SELECT  DATE_FORMAT(date,'%Y-%m') AS date FROM expenses GROUP BY DATE_FORMAT(date,'%Y-%m')) as date FROM expenses GROUP BY purchasetype";
-$this->result2 =  $this->db->pdo->query($sql)->fetch();
-var_dump($this->result2);
+// $this->result2 = 
+echo '<pre>';
+var_dump($this->db->pdo->query($sql)->fetch());
+// var_dump($this->result2);
 
       }
       public function selectTag(){
