6dadb77d3e85b660307b4e8ed724bf40f7cb32d3
ernest romen <ernestromen1996@gmail.com>
2021-07-17 15:28:28 +0300

    change

--- index.php
+++ index.php
@@ -193,7 +193,7 @@ $this->db->pdo->query($sql);
 
 
     
-$sql = "SELECT id,purchasetype,amount,date FROM expenses WHERE DATE_FORMAT(date,'%m') =MONTH(GETDATE());";
+$sql = "SELECT id,purchasetype,amount,date FROM expenses WHERE DATE_FORMAT(date,'%m') =MONTH(NOW());";
 
 $this->result = $this->db->pdo->query($sql)->fetchall();
 
