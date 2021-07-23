0efc05824ea0c0ba1f5097d824046d92a5b78f80
ernest romen <ernestromen1996@gmail.com>
2021-07-23 17:26:46 +0300

    adding salary display

--- index.php
+++ index.php
@@ -136,7 +136,7 @@ if($w === 'amount'){
 $this->db->pdo->query($sql);
 }else if($w === 'submitSalary'){
 //insert value from salary and source
-$sql = "INSERT INTO salary (source, amount) VALUES ('$res','$res2')";
+// $sql = "INSERT INTO salary (source, amount) VALUES ('$res','$res2')";
 $sql = "UPDATE salary SET source ='$res' ,SET amount ='$res2' WHERE = 5;"
 
 $this->db->pdo->query($sql);
