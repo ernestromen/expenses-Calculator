127e34e955632f239fec6981f9605af85be4e894
ernest romen <ernestromen1996@gmail.com>
2023-07-12 06:17:25 +0300

    the rest

--- index.php
+++ index.php
@@ -160,11 +160,8 @@ $crud->show();
         </tr>
       </tbody>
     </table>
-    <button class="btn btn-primary" @click="getExpenseByPurchaseType">Click me</button>
-
   </div>
   </div>
-
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
     integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
     crossorigin="anonymous"></script>
--- index2.php
+++ index2.php
@@ -24,27 +24,36 @@ class CRUD extends DB
         return $this->result;
     }
 
-    public function getByPurchaseType()
+    public function getByPurchaseType($searchVariable)
     {
-        $sql = "select purchasetype,amount,created_at from expenses where purchasetype = 'food'";
+        $sql = "select purchasetype,amount,created_at from expenses where purchasetype = '$searchVariable'";
         // --  WHERE DATE_FORMAT(created_at,'%m') =MONTH(NOW());";
         $this->result = $this->db->pdo->query($sql)->fetchall();
 
         return $this->result;
     }
-
+    public function insertExpense($amount, $purchaseType)
+    {
+        $sql = "INSERT INTO expenses (purchasetype,amount,created_at) VALUES('$purchaseType','$amount',NOW())";
+        $this->db->pdo->query($sql);
+    }
 }
 
-
 $res = new DB('localhost', 'db0123', 'root', '');
-
 $crud = new CRUD($res);
+$type = isset($_GET['type']);
 
-if (isset($_GET['type'])) {
-    // The 'type' query parameter exists
-$data = $crud->getByPurchaseType();
-echo json_encode(['expenses' => $data]);
-} else {
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['type'])) {
+    $data = $crud->getByPurchaseType($_GET['type']);
+    echo json_encode(['expenses' => $data]);
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $data = $crud->show();
     echo json_encode(['expenses' => $data]);
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents("php://input"), true);
+    $selectedOptionAddingExpense = $data['selectedOptionAddingExpense'];
+    $typedAmount = $data['typedAmount'];
+    $crud->insertExpense($typedAmount, $selectedOptionAddingExpense);
+    $data = $crud->show();
+    echo json_encode(["expenses" => $data]);
 }
